43e43fb5de7f1ee3faedda0f9572caaecbd7fbe6
terry0803 <145676275+terry0803@users.noreply.github.com>
2023-12-26 01:58:22 +0800

    Add files via upload

--- 星輝網網站/js/Slide.js
+++ 星輝網網站/js/Slide.js
@@ -1,44 +1,43 @@
 let slideIndex = 0;
-showSlides();  // 啟動自動輪播
+let slideTimeout;
 
-var slideTimeout;
+// 启动自动轮播
+showSlides();
 
-function showSlides() {
-  let i;
+// 显示指定索引的幻灯片
+function showSlides(auto = true) {
   let slides = document.getElementsByClassName('mySlideshows');
   let dots = document.getElementsByClassName('dot');
 
-  // 隱藏所有幻燈片
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = 'none';  
-  }
-
-  // 重置幻燈片索引
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-
-  // 更新點點狀態
-  for (i = 0; i < dots.length; i++) {
+  // 隐藏所有幻灯片
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
     dots[i].className = dots[i].className.replace(' active', '');
   }
 
-  // 顯示當前幻燈片並激活對應的點點
-  slides[slideIndex-1].style.display = 'block';  
-  dots[slideIndex-1].className += ' active';
+  // 自动播放时递增 slideIndex
+  if (auto) {
+    slideIndex++;
+  }
 
-  // 設置自動切換幻燈片的時間間隔
+  if (slideIndex >= slides.length) { slideIndex = 0; }
+  if (slideIndex < 0) { slideIndex = slides.length - 1; }
+
+  slides[slideIndex].style.display = 'block';
+  dots[slideIndex].className += ' active';
+
   clearTimeout(slideTimeout);
-  slideTimeout = setTimeout(showSlides, 2000); // 2秒後切換幻燈片
+  slideTimeout = setTimeout(function() { showSlides(); }, 2000); // 2秒后自动切换
 }
 
-// 手動切換幻燈片
+// 手动切换幻灯片
 function moveSlide(n) {
-  slideIndex += n - 1;
-  showSlides();
+  slideIndex += n;
+  showSlides(false); // 手动切换，不自动递增 slideIndex
 }
 
+// 设置当前幻灯片
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+  slideIndex = n - 1;
+  showSlides(false); // 手动切换，不自动递增 slideIndex
 }
-
-
